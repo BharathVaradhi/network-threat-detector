44e81ce7598ef6785dbd057efec858bc98d2eabf
Bharath <varadhibharath123@gmail.com>
2026-06-16 15:36:33 +0530

    Complete packet capture and CSV storage

---
+++ captures.py
@@ -0,0 +1,65 @@
+import csv
+import os
+from scapy.all import sniff, IP, TCP, UDP
+from datetime import datetime
+
+
+def packet_callback(packet):
+
+    if IP in packet:
+
+        packet_data = {
+            "source_ip": packet[IP].src,
+            "destination_ip": packet[IP].dst,
+            "protocol": None,
+            "source_port": None,
+            "destination_port": None,
+            "packet_size": len(packet),
+            "timestamp": datetime.now().isoformat()
+        }
+
+        if TCP in packet:
+            packet_data["protocol"] = "TCP"
+            packet_data["source_port"] = packet[TCP].sport
+            packet_data["destination_port"] = packet[TCP].dport
+
+        elif UDP in packet:
+            packet_data["protocol"] = "UDP"
+            packet_data["source_port"] = packet[UDP].sport
+            packet_data["destination_port"] = packet[UDP].dport
+
+        with open(csv_file, "a", newline="") as file:
+            writer = csv.writer(file)
+
+            writer.writerow([
+                packet_data["source_ip"],
+                packet_data["destination_ip"],
+                packet_data["protocol"],
+                packet_data["source_port"],
+                packet_data["destination_port"],
+                packet_data["packet_size"],
+                packet_data["timestamp"]
+            ])
+
+        print("\n{")
+
+        for key, value in packet_data.items():
+            print(f"{key}: {value}")
+
+        print("}")
+
+csv_file = "packets.csv"
+
+if not os.path.exists(csv_file):
+    with open(csv_file, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([
+            "source_ip",
+            "destination_ip",
+            "protocol",
+            "source_port",
+            "destination_port",
+            "packet_size",
+            "timestamp"
+        ])
+sniff(prn=packet_callback, count=10)
